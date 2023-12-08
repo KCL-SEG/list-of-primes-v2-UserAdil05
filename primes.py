f7d91d10f2dff9520deff4e733efa0e69949ac77
UserAdil05 <k20056435@kcl.ac.uk>
2023-12-08 15:41:53 +0000

    excersice compe,lted

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
-    list = []
-    return list
+  
+
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+
+    def is_prime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    primes_list = []
+    num = 2
+    while len(primes_list) < number_of_primes:
+        if is_prime(num):
+            primes_list.append(num)
+        num += 1
+
+    return primes_list
+primes(10), primes(1), primes(20)  # examples from the test cases
+
